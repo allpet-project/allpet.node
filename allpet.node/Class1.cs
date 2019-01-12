5ac3946e13eec55842235112325aa0a3c1c2c1dc
lights <lightsever@hotmail.com>
2019-01-12 22:03:31 +0800

    add nodeproj

---
+++ allpet.node/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using allpet.peer.tcp;
+
+namespace allpet.node
+{
+    public interface INode : IDisposable
+    {
+        void InitChain();//链初始化
+
+        void StartNetwork();//启动网络
+
+
+        UInt64 GetBlockCount();
+        //获取节点对象
+        allpet.peer.tcp.IPeer peer
+        {
+            get;
+        }
+    }
+    public class Node
+    {
+        static INode CreateNode()
+        {
+            return new PeerNode();
+        }
+    }
+    class PeerNode : INode
+    {
+        public IPeer peer => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ulong GetBlockCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InitChain()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void StartNetwork()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
